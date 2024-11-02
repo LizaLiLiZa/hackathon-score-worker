9ce8e4ad5803026608078b4818edeac556665cd0
ejik-sasha <112908553+ejik-sasha@users.noreply.github.com>
2024-11-03 00:18:35 +0300

    all

--- all/main.py
+++ all/main.py
@@ -4,7 +4,7 @@ from promts import prepare_prompt
 
 from model import evaluate_reviews_with_llm
 
-file_path = "sample_reviews.json"
+file_path = "./all/sample_reviews.json"
 reviews = load_reviews(file_path)
 
 promt_1 = prepare_prompt(reviews)
