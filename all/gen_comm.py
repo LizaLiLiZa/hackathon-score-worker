aa0e7763fd4ca060d8b3c7eb087fa9f4d61628b7
ejik-sasha <112908553+ejik-sasha@users.noreply.github.com>
2024-11-02 23:08:01 +0300

    aaaaaa but with testing

--- all/gen_comm.py
+++ all/gen_comm.py
@@ -1,16 +1,29 @@
 import json
 import os
 
-def get_comm_add(id_to, id_from, gen_comm):
+def gen_comm_add(id_to, id_from, gen_comm):
     if not os.path.exists("gen_comm.json"):
-        with open("gen_comm.json", "w", encoding="utf-8") as f:
-            json.dump([], f)
+        try:
+            with open("gen_comm.json", "w", encoding="utf-8") as f:
+                json.dump([], f)
+        except IOError as e:
+            print("Ошибка при создании файла:{e}")
+    try:
+        with open("gen_comm.json", "r+", encoding="utf-8") as f:
+            try:
+                data = json.load(f)
+            except json.JSONDecodeError:
+                print("Ошибка чтения из json файла! Файл может быть поврежден!")     
+            data.append({"id_to": id_to, "id_from": id_from, "gen_comm": gen_comm})
+            f.seek(0)
+            try:
+                json.dump(data, f, indent=4)
+            except IOError as e:
+                print("Ошибка при записи данных в файл:{e}")
+    except IOError as e:
+            print("Ошибка при чтении/записи данных в файл:{e}")
 
-    with open("gen_comm.json", "r+", encoding="utf-8") as f:
-        data = json.load(f)
-        data.append({"id_to": id_to, "id_from": id_from, "gen_comm": gen_comm})
-        f.seek(0)
-        json.dump(data, f, indent=4)
+
 
 
     with open("gen_comm.json", "r", encoding="utf-8") as f:
